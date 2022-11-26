374d1fd775f69cb7086bb1e4c22abe516acfcce3
aeither <36173828+aeither@users.noreply.github.com>
2022-11-26 23:47:56 +0100

    add ui deps

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          // eslint-disable-next-line @typescript-eslint/no-var-requires
+          ...require("daisyui/src/colors/themes")["[data-theme=dark]"],
+          primary: "#F50029",
+          secondary: "#f6d860",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+    ],
   },
-  plugins: [],
+  theme: {
+    extend: {
+      colors: {
+        "neutral-dark": "#14171A",
+        "neutral-medium": "#1A1E23",
+        "neutral-light": "#68778D",
+        primary: "#F50029",
+        white: "#FFF",
+      },
+    },
+  },
+  plugins: [require("daisyui")],
 };
