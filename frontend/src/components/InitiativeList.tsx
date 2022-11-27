81a2d04e780ed67329f2bbee663754dc3377cea8
aeither <36173828+aeither@users.noreply.github.com>
2022-11-27 17:48:10 +0100

    fund initiative

---
+++ frontend/src/components/InitiativeList.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { useContract, useNFTs } from "@thirdweb-dev/react";
+import type { FC } from "react";
+import { INITIATIVE_ADDRESS } from "../utils/constants";
+import type { CardProps } from "./GotchiCard";
+import GotchiCard from "./GotchiCard";
+
+const InitiativeList: FC = () => {
+  const { contract } = useContract(INITIATIVE_ADDRESS, "edition-drop");
+  const {
+    data: nfts,
+    isLoading,
+    refetch,
+  } = useNFTs(contract, { start: 0, count: 100 });
+
+  return (
+    <div>
+      <div className="flex w-full flex-col">
+        <h2 className="text-xl font-bold text-white">Trending projects</h2>
+        <p className="text-neutral-light">Most traction in the last 24 hours</p>
+      </div>
+
+      <div className="grid grid-cols-3 gap-8">
+        {!isLoading && nfts ? (
+          nfts.map((nft) => {
+            const data: CardProps = {
+              title: String(nft.metadata.name) || "",
+              description: nft.metadata.description || "",
+              image: nft.metadata.animation_url || "",
+              btnText: "Fund 5 USDC",
+              btnAction: async () => {
+                if (contract) {
+                  await contract.claim(nft.metadata.id, 1);
+                  refetch();
+                }
+              },
+            };
+
+            return (
+              <>
+                <GotchiCard {...data} />
+              </>
+            );
+          })
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default InitiativeList;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { type NextPage } from "next";
-import Card from "../components/Card";
+import InitiativeList from "../components/InitiativeList";
 import Navbar from "../components/Navbar";
 
 const Home: NextPage = () => {
@@ -7,7 +7,7 @@ const Home: NextPage = () => {
     <>
       <Navbar />
       <main className="flex min-h-[calc(100vh-80px)] flex-col items-center justify-center bg-neutral-dark">
-        <div className="flex flex-col items-center justify-center gap-8 px-4 py-16 ">
+        <div className="container flex flex-col items-center justify-center gap-8 px-4 py-16 ">
           <div className="flex w-full items-center justify-between pb-12">
             <div>
               <h1 className="text-5xl font-extrabold italic tracking-tight text-white sm:text-[5rem]">
@@ -21,17 +21,7 @@ const Home: NextPage = () => {
               <img src="https://placeimg.com/400/225/arch" alt="car!" />
             </div>
           </div>
-          <div className="flex w-full flex-col">
-            <h2 className="text-xl font-bold text-white">Trending projects</h2>
-            <p className="text-neutral-light">
-              Most traction in the last 24 hours
-            </p>
-          </div>
-          <div className="grid grid-cols-3 gap-8">
-            <Card />
-            <Card />
-            <Card />
-          </div>
+          <InitiativeList />
         </div>
       </main>
     </>
