467f65771f93c9b9eac23dda2e1b01aa648e094e
aeither <36173828+aeither@users.noreply.github.com>
2022-11-27 11:21:31 +0100

    improvements

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -2,15 +2,17 @@ import type { FC } from "react";
 
 const Card: FC = () => {
   return (
-    <div className="card w-auto bg-neutral-medium border border-neutral-medium transition duration-300 hover:border-white">
+    <div className="card w-auto border border-neutral-medium bg-neutral-dark transition duration-300 hover:border-white">
       <figure>
         <img src="https://placeimg.com/400/225/arch" alt="car!" />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">title</h2>
-        <p>description</p>
+        <h2 className="card-title text-white">title</h2>
+        <p className="text-neutral-light">description</p>
         <div className="card-actions justify-end">
-          <button className="btn-primary btn w-full">Fund 1$ USDC</button>
+          <button className="btn w-full bg-neutral-dark text-primary hover:border-neutral-light">
+            Fund 1$ USDC
+          </button>
         </div>
       </div>
     </div>
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -7,13 +7,26 @@ const Home: NextPage = () => {
     <>
       <Navbar />
       <main className="flex min-h-[calc(100vh-80px)] flex-col items-center justify-center bg-neutral-dark">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold italic tracking-tight text-white sm:text-[5rem]">
-            <span className="text-primary">OP</span> Gotchis
-          </h1>
-          <p className="text-neutral-light">
-            Fund public good initiatives and receive an OP Gotchi egg.
-          </p>
+        <div className="flex flex-col items-center justify-center gap-8 px-4 py-16 ">
+          <div className="flex w-full items-center justify-between pb-12">
+            <div>
+              <h1 className="text-5xl font-extrabold italic tracking-tight text-white sm:text-[5rem]">
+                <span className="text-primary">OP</span> Gotchis
+              </h1>
+              <p className="pt-4 text-lg text-neutral-light">
+                Fund public good initiatives and receive an OP Gotchi egg.
+              </p>
+            </div>
+            <div>
+              <img src="https://placeimg.com/400/225/arch" alt="car!" />
+            </div>
+          </div>
+          <div className="flex w-full flex-col">
+            <h2 className="text-xl font-bold text-white">Trending projects</h2>
+            <p className="text-neutral-light">
+              Most traction in the last 24 hours
+            </p>
+          </div>
           <div className="grid grid-cols-3 gap-8">
             <Card />
             <Card />
