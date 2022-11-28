6e737faa951d94c13f4fc3daf8892e8611d98bee
aeither <36173828+aeither@users.noreply.github.com>
2022-11-28 16:05:52 +0100

    improve play page

--- frontend/src/components/BattlefieldHead.tsx
+++ frontend/src/components/BattlefieldHead.tsx
@@ -8,22 +8,30 @@ const BattlefieldHead: FC = () => {
 
   return (
     <div className="grid grid-cols-3">
-      <img src="https://placeimg.com/400/225/arch" alt="car!" />
-      <div className="text-center">
+      <div className="p-12">
+        <img className="" src="/attack.png" alt="car!" />
+      </div>
+      <div className="self-center text-center">
         <h1 className="text-5xl font-extrabold italic tracking-tight text-white sm:text-[5rem]">
           <span className="text-primary">Battlefield</span>
         </h1>
-        <p className="text-neutral-light">Instructions:</p>
-        <p className="text-neutral-light">1. Choose a Gotchi.</p>
-        <p className="text-neutral-light">
-          2. Choose a type to attack the enemy.
-        </p>
-        <p className="text-neutral-light">3. The enemy will block 3 types.</p>
-        <p className="text-neutral-light">
-          You win if the chosen color is not blocked.
-        </p>
+        <div className="pt-8">
+          <p className="text-neutral-light">Instructions:</p>
+          <p className="text-neutral-light">1. Select your Gotchi.</p>
+          <p className="text-neutral-light">
+            2. Choose a type to attack the enemy.
+          </p>
+          <p className="text-neutral-light">
+            3. A random enemy will spawn with a type weakness.
+          </p>
+          <p className="text-neutral-light">
+            You win if you attacked the enemy&apos;s weakness.
+          </p>
+        </div>
       </div>
-      <img src="https://placeimg.com/400/225/arch" alt="car!" />
+      <div className="p-12">
+        <img src="/defense.png" alt="car!" />
+      </div>
     </div>
   );
 };
